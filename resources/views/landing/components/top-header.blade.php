93e43b89adec309fce63dc8bd248a5c571d58d0a
RAJEEV <RAJEEV@LAPTOP-5MMTIT2T>
2023-05-14 21:09:08 +0530

    address

--- resources/views/landing/components/top-header.blade.php
+++ resources/views/landing/components/top-header.blade.php
@@ -3,7 +3,7 @@
       <div class="row">
           <div class="col-lg-8 col-md-8">
             <ul class="top-info text-center text-md-left">
-                <li><i class="fas fa-map-marker-alt"></i> <p class="info-text">9051 Constra Incorporate, USA</p>
+                <li><i class="fas fa-map-marker-alt"></i> <p class="info-text">{{$page['address']}}</p>
                 </li>
             </ul>
           </div>
